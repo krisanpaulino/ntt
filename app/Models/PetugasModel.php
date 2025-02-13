2fe6439bf65e406453e3bc313fde64a4bafb5f13
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2025-02-13 12:38:59 +0800

    Update PetugasModel.php

--- app/Models/PetugasModel.php
+++ app/Models/PetugasModel.php
@@ -75,7 +75,7 @@ class PetugasModel extends Model
     {
 
         $this->join('user', 'user.user_id = petugas.user_id');
-        $this->join('kelurahan', 'kelurahan.kelurahan_id = petugas.kelurahan_id');
+        $this->join('kelurahan', 'kelurahan.kelurahan_id = posyandu.kelurahan_id');
         $this->join('posyandu', 'posyandu.posyandu_id = petugas.posyandu_id');
         if ($user_id != null) {
             $this->where('petugas.user_id', $user_id);
