fe57b470de49f6ea2655a3c006c228e31b3b5958
krisanpaulino <36087959+krisanpaulino@users.noreply.github.com>
2025-03-06 14:13:53 +0800

    Update User.php

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -236,7 +236,7 @@ class User extends BaseController
         // dd($data);
         $model->where('petugasdesa_id', $petugasdesa_id);
         $model->update($petugasdesa_id, $data);
-        dd($model->errors());
+        // dd($model->errors());
         return redirect()->to(previous_url())->with('success', 'Data berhasil diubah');
     }
     public function deletePetugasdesa()
